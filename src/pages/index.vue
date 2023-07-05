92769c1dbb6f682a9a2f64431183288474489bde
Sgss123 <103869422+Sgss123@users.noreply.github.com>
2023-07-06 01:46:52 +0800

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,5 @@
 <template>
     <h1 class="font-serif text-6xl text-center">Home page</h1>
+    <img src="https://v2.jinrishici.com/one.svg?font-size=50&spacing=2&color=DarkRed" alt="" class="w-full">
     <img src="https://1.116.141.20/" alt="IP" class="mt-4 w-160 h-90 z-0">
 </template>
