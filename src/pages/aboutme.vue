560a6abd0971fa0c311e6c5061d88a862badb261
Sgss123 <103869422+Sgss123@users.noreply.github.com>
2023-07-06 01:43:08 +0800

    小修改

--- src/pages/aboutme.vue
+++ src/pages/aboutme.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1 class="font-sans text-6xl text-center">This is a about page.</h1>
+    <div>
+        <h1 class="font-sans text-6xl text-center mb-4">关于我</h1>
+        <div class="firstPage max-w-4xl">
+            <el-row>
+                <el-col :span="12">
+                    <div><img src="https://sdfsdf.dev/240x340.jpg,green,yellow" alt="" class="pr-5"/></div>
+                </el-col>
+                <el-col :span="12">
+                    <div>
+                        <p class="text-dark-200">网名Sgss，一名学生。</p>
+                    </div>
+                </el-col>
+            </el-row>
+
+        </div>
+
+    </div>
 </template>
+<style></style>
--- src/pages/root.vue
+++ src/pages/root.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-    <div class="min-h-screen items-center flex flex-col max-w-5xl mx-auto">
-        <ul class="flex bg-slate-200 rounded px-8 py-4 text-lg w-full justify-center gap-x-8">
+    <div class="min-h-screen items-center flex flex-col mx-auto">
+        <ul class="flex bg-slate-200 rounded px-8 py-4 text-lg w-full justify-center gap-x-8 max-w-5xl">
             <li><a href="#" class="text-blue-500 hover:(text-blue-700)">主页</a></li>
             <li><a href="#/about" class="text-blue-500 hover:(text-blue-700)">关于</a></li>
             <li><a href="https://blog.waterspo.top" class="text-blue-500 hover:(text-blue-700)">Blog</a></li>
