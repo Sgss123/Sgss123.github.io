2d2f98e7ff5bde7b53a4d18e07be2ee044a24b3d
Sgss123 <103869422+Sgss123@users.noreply.github.com>
2023-07-12 12:38:55 +0800

    修改IP图片

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,5 @@
 <template>
     <h1 class="font-serif text-6xl text-center">Home page</h1>
     <img src="https://v2.jinrishici.com/one.svg?font-size=50&spacing=2&color=DarkRed" alt="" class="w-full">
-    <img src="https://1.116.141.20/" alt="IP" class="mt-4 w-160 h-90 z-0">
+    <img src="https://api.oick.cn/netcard/api.php" alt="IP" class="mt-4 w-160 h-90 z-0">
 </template>
