a9d8cfc836b6ba55f44a51ff1aafef8199280886
Sgss <103869422+Sgss123@users.noreply.github.com>
2024-04-21 10:39:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import WindiCSS from 'vite-plugin-windicss'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+    base:'/',
     resolve: {
         alias: {
             "~": path.resolve(__dirname, "src")
