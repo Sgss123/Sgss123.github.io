d96b980b298b650df36b6702dfe90bc5df1fef55
Sgss <103869422+Sgss123@users.noreply.github.com>
2024-08-16 18:42:36 +0800

    Prepare to rebuild

--- src/pages/404.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <el-result icon="error" sub-title="错误代码：404 Not Found" title="网页未找到">
-      <template #extra>
-        <el-button type="primary" @click="$router.push('/')">返回首页</el-button>
-      </template>
-    </el-result>
-  </div>
-</template>
