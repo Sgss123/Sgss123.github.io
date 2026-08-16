80fbd7335bad00d53a1ec1264b8c130c8a091bb2
MFJip612 <103869422+MFJip612@users.noreply.github.com>
2026-08-17 00:12:18 +0800

    refactor: remove unused pages and SSG configuration files
    - Deleted article page metadata file to clean up unused code.
    - Removed index view file which contained the main page layout and logic.
    - Eliminated page metadata file for the home page to streamline the project.
    - Removed SSG configuration files as they are no longer needed.
    - Deleted TypeScript configuration files related to the application and Node.js environment.
    - Cleaned up the Vite SSG plugin and configuration files to reflect the removal of SSG functionality.

--- pages/+Head.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <meta name="google-adsense-account" content="ca-pub-3433477986485987">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3433477986485987"
-        crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/8739c50b73.js" crossorigin="anonymous"></script>
-    <title>MFJip生活随笔</title>
-</template>
--- src/views/article/page.ts
+++
@@ -1,9 +0,0 @@
-import type { PageMeta } from '@/types'
-
-const meta: PageMeta = {
-  title: '文章列表',
-  menuOrder: 2,
-  isSSG: true,
-}
-
-export default meta
